9068c07a15b9856bfad3789d58cad907e2458182
handy <13546606929@163.com>
2020-12-11 15:52:56 +0800

    解决异常都在自己本身解决

--- src/main/java/com/handy/lib/util/YamlHelper.java
+++ src/main/java/com/handy/lib/util/YamlHelper.java
@@ -14,96 +14,104 @@ import java.util.List;
  */
 public class YamlHelper {
 
-    public static <T> T readYaml(String fileName, Class<T> clazz) throws IllegalAccessException, InstantiationException {
+    public static <T> T readYaml(String fileName, Class<T> clazz) {
         return readYaml(new File(fileName), clazz);
-
     }
 
-    public static <T> T readYaml(File file, Class<T> clazz) throws IllegalAccessException, InstantiationException {
+    public static <T> T readYaml(File file, Class<T> clazz) {
         return readYaml(YamlConfiguration.loadConfiguration(file), clazz);
 
     }
 
-    public static <T> T readYaml(YamlConfiguration yaml, Class<T> clazz) throws IllegalAccessException, InstantiationException {
+    public static <T> T readYaml(YamlConfiguration yaml, Class<T> clazz) {
         return readYaml(yaml, clazz, "");
     }
 
-    public static <T> T readYaml(YamlConfiguration yaml, Class<T> clazz, String path) throws IllegalAccessException, InstantiationException {
-        T o = clazz.newInstance();
-        Class<?> q = o.getClass();
-        for (Field field : q.getDeclaredFields()) {
-            field.setAccessible(true);
-            Class<?> c = field.getType();
-            if (field.getAnnotation(NoAutowired.class) == null) {
-                YamlField f = field.getAnnotation(YamlField.class);
-                if ((c.isPrimitive() ||
-                        c.equals(String.class) ||
-                        c.equals(List.class) ||
-                        c.equals(Byte.class) ||
-                        c.equals(Long.class) ||
-                        c.equals(Double.class) ||
-                        c.equals(Float.class) ||
-                        c.equals(Character.class) ||
-                        c.equals(Short.class) ||
-                        c.equals(Boolean.class))) {
-                    String p = "".equalsIgnoreCase(path) ? (f == null ? field.getName() : f.name()) : path + "." + (f == null ? field.getName() : f.name());
-                    field.set(o, yaml.get(p));
+    public static <T> T readYaml(YamlConfiguration yaml, Class<T> clazz, String path) {
+        T o = null;
+        try {
+            o = clazz.newInstance();
+            Class<?> q = o.getClass();
+            for (Field field : q.getDeclaredFields()) {
+                field.setAccessible(true);
+                Class<?> c = field.getType();
+                if (field.getAnnotation(NoAutowired.class) == null) {
+                    YamlField f = field.getAnnotation(YamlField.class);
+                    if ((c.isPrimitive() ||
+                            c.equals(String.class) ||
+                            c.equals(List.class) ||
+                            c.equals(Byte.class) ||
+                            c.equals(Long.class) ||
+                            c.equals(Double.class) ||
+                            c.equals(Float.class) ||
+                            c.equals(Character.class) ||
+                            c.equals(Short.class) ||
+                            c.equals(Boolean.class))) {
+                        String p = "".equalsIgnoreCase(path) ? (f == null ? field.getName() : f.name()) : path + "." + (f == null ? field.getName() : f.name());
+                        field.set(o, yaml.get(p));
 
-                } else {
-                    String p = "".equalsIgnoreCase(path) ? (f == null ? field.getName() : f.name()) : path + "." + (f == null ? field.getName() : f.name());
-                    field.set(o, readYaml(yaml, c, p));
+                    } else {
+                        String p = "".equalsIgnoreCase(path) ? (f == null ? field.getName() : f.name()) : path + "." + (f == null ? field.getName() : f.name());
+                        field.set(o, readYaml(yaml, c, p));
 
+                    }
                 }
-
             }
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
         }
         return o;
     }
 
     public static void writeYaml(String fileName, Object object) {
+        writeYaml(new File(fileName), object);
+    }
+
+    public static void writeYaml(File file, Object object) {
         try {
-            writeYaml(new File(fileName), object);
-        } catch (IllegalAccessException | IOException e) {
+            YamlConfiguration yaml = YamlConfiguration.loadConfiguration(file);
+            writeYaml(yaml, object);
+            yaml.save(file);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void writeYaml(File file, Object object) throws IllegalAccessException, IOException {
-        YamlConfiguration yaml = YamlConfiguration.loadConfiguration(file);
-        writeYaml(yaml, object);
-        yaml.save(file);
-
-    }
-
-    public static void writeYaml(YamlConfiguration yaml, Object object) throws IllegalAccessException {
+    public static void writeYaml(YamlConfiguration yaml, Object object) {
         writeYaml(yaml, object, "");
     }
 
-    private static void writeYaml(YamlConfiguration yaml, Object object, String path) throws IllegalAccessException {
-        Class<?> clazz = object.getClass();
-        for (Field field : clazz.getDeclaredFields()) {
-            field.setAccessible(true);
-            Class<?> c = field.getType();
-            if (field.getAnnotation(NoAutowired.class) == null) {
-                YamlField f = field.getAnnotation(YamlField.class);
-                if ((c.isPrimitive() ||
-                        c.equals(String.class) ||
-                        c.equals(List.class) ||
-                        c.equals(Byte.class) ||
-                        c.equals(Long.class) ||
-                        c.equals(Double.class) ||
-                        c.equals(Float.class) ||
-                        c.equals(Character.class) ||
-                        c.equals(Short.class) ||
-                        c.equals(Boolean.class))) {
-                    String p = "".equalsIgnoreCase(path) ? (f == null ? field.getName() : f.name()) : path + "." + (f == null ? field.getName() : f.name());
-                    yaml.set(p, field.get(object));
-                } else {
-                    String p = "".equalsIgnoreCase(path) ? (f == null ? field.getName() : f.name()) : path + "." + (f == null ? field.getName() : f.name());
-                    writeYaml(yaml, field.get(object), p);
+    private static void writeYaml(YamlConfiguration yaml, Object object, String path) {
+        try {
+            Class<?> clazz = object.getClass();
+            for (Field field : clazz.getDeclaredFields()) {
+                field.setAccessible(true);
+                Class<?> c = field.getType();
+                if (field.getAnnotation(NoAutowired.class) == null) {
+                    YamlField f = field.getAnnotation(YamlField.class);
+                    if ((c.isPrimitive() ||
+                            c.equals(String.class) ||
+                            c.equals(List.class) ||
+                            c.equals(Byte.class) ||
+                            c.equals(Long.class) ||
+                            c.equals(Double.class) ||
+                            c.equals(Float.class) ||
+                            c.equals(Character.class) ||
+                            c.equals(Short.class) ||
+                            c.equals(Boolean.class))) {
+                        String p = "".equalsIgnoreCase(path) ? (f == null ? field.getName() : f.name()) : path + "." + (f == null ? field.getName() : f.name());
 
+                        yaml.set(p, field.get(object));
+
+                    } else {
+                        String p = "".equalsIgnoreCase(path) ? (f == null ? field.getName() : f.name()) : path + "." + (f == null ? field.getName() : f.name());
+                        writeYaml(yaml, field.get(object), p);
+
+                    }
                 }
             }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
         }
     }
 
