2bf8bb36fa769c3e4dfc450273d975101f672787
handy <13546606929@163.com>
2021-12-22 20:38:08 +0800

    2.0.8

--- src/main/java/com/handy/lib/InitApi.java
+++ src/main/java/com/handy/lib/InitApi.java
@@ -45,7 +45,7 @@ import java.util.stream.Collectors;
 public class InitApi {
     public static Plugin PLUGIN;
     private static ClassUtil CLASS_UTIL;
-    private final static String VERSION = "2.0.6";
+    private final static String VERSION = "2.0.8";
 
     private InitApi() {
     }
