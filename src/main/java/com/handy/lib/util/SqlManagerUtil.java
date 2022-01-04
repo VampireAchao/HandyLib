50129661cdf0b98155122d9324dce0a0e83aad5d
韩帅 <shuai.han@ersoft.cn>
2022-01-04 13:33:25 +0800

    2.5.2 修复获取sqlite连接池异常

--- src/main/java/com/handy/lib/util/SqlManagerUtil.java
+++ src/main/java/com/handy/lib/util/SqlManagerUtil.java
@@ -1,14 +1,12 @@
 package com.handy.lib.util;
 
 import com.handy.lib.InitApi;
+import com.handy.lib.api.MessageApi;
 import com.handy.lib.constants.BaseConstants;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Date;
 
 /**
@@ -41,23 +39,35 @@ public class SqlManagerUtil {
      * @param storageMethod 连接方式
      */
     public void enableTable(String storageMethod) {
-        HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setPoolName(InitApi.PLUGIN.getName() + "HikariPool");
-        if (BaseConstants.MYSQL.equalsIgnoreCase(storageMethod)) {
-            String host = BaseConstants.STORAGE_CONFIG.getString("MySQL.Host");
-            String database = BaseConstants.STORAGE_CONFIG.getString("MySQL.Database");
-            int port = BaseConstants.STORAGE_CONFIG.getInt("MySQL.Port");
-            String useSsl = BaseConstants.STORAGE_CONFIG.getString("MySQL.UseSSL");
-            String jdbcUrl = "jdbc:mysql://" + host + ":" + port + "/" + database + "?useSSL=" + useSsl + "&useUnicode=true&characterEncoding=UTF-8";
-            hikariConfig.setJdbcUrl(jdbcUrl);
-            hikariConfig.setUsername(BaseConstants.STORAGE_CONFIG.getString("MySQL.User"));
-            hikariConfig.setPassword(BaseConstants.STORAGE_CONFIG.getString("MySQL.Password"));
-        } else {
-            String jdbcUrl = "jdbc:sqlite:" + InitApi.PLUGIN.getDataFolder().getAbsolutePath() + "/" + InitApi.PLUGIN.getName() + ".db";
-            hikariConfig.setDriverClassName("org.sqlite.JDBC");
-            hikariConfig.setJdbcUrl(jdbcUrl);
+        try {
+            HikariConfig hikariConfig = new HikariConfig();
+            hikariConfig.setPoolName(InitApi.PLUGIN.getName() + "HikariPool");
+            if (BaseConstants.MYSQL.equalsIgnoreCase(storageMethod)) {
+                String host = BaseConstants.STORAGE_CONFIG.getString("MySQL.Host");
+                String database = BaseConstants.STORAGE_CONFIG.getString("MySQL.Database");
+                int port = BaseConstants.STORAGE_CONFIG.getInt("MySQL.Port");
+                String useSsl = BaseConstants.STORAGE_CONFIG.getString("MySQL.UseSSL");
+                String jdbcUrl = "jdbc:mysql://" + host + ":" + port + "/" + database + "?useSSL=" + useSsl + "&useUnicode=true&characterEncoding=UTF-8";
+                hikariConfig.setJdbcUrl(jdbcUrl);
+                hikariConfig.setUsername(BaseConstants.STORAGE_CONFIG.getString("MySQL.User"));
+                hikariConfig.setPassword(BaseConstants.STORAGE_CONFIG.getString("MySQL.Password"));
+            } else {
+                String jdbcUrl = "jdbc:sqlite:" + InitApi.PLUGIN.getDataFolder().getAbsolutePath() + "/" + InitApi.PLUGIN.getName() + ".db";
+                hikariConfig.setDriverClassName("org.sqlite.JDBC");
+                hikariConfig.setJdbcUrl(jdbcUrl);
+            }
+            ds = new HikariDataSource(hikariConfig);
+        } catch (Throwable throwable) {
+            if (BaseConstants.DEBUG) {
+                throwable.printStackTrace();
+            }
+            MessageApi.sendConsoleDebugMessage("sqlite 初始化连接异常，使用jdbc方法获取链接");
+            try {
+                Class.forName("org.sqlite.JDBC");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
         }
-        ds = new HikariDataSource(hikariConfig);
     }
 
     /**
@@ -68,9 +78,14 @@ public class SqlManagerUtil {
      * @throws SQLException 异常
      */
     public Connection getConnection(String storageMethod) throws SQLException {
+        if (ds == null && BaseConstants.SQLITE.equalsIgnoreCase(storageMethod)) {
+            MessageApi.sendConsoleDebugMessage("sqlite HikariDataSource 获取链接异常，使用jdbc方法获取链接");
+            return DriverManager.getConnection("jdbc:sqlite:" + InitApi.PLUGIN.getDataFolder().getAbsolutePath() + "/" + InitApi.PLUGIN.getName() + ".db");
+        }
         if (this.getStorageMethod().equalsIgnoreCase(storageMethod)) {
             // 如果链接被关闭了，就重新打开
             if (ds.isClosed()) {
+                MessageApi.sendConsoleDebugMessage(" HikariDataSource 链接异常关闭，重新打开");
                 enableTable(storageMethod);
             }
             return ds.getConnection();
