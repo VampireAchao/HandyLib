5a4bd476e91ea623ff06e34f5b2ae4f61e23a25c
handy <13546606929@163.com>
2022-01-05 20:14:30 +0800

    2.5.7 获取材质使用原生办法

--- src/main/java/com/handy/lib/expand/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * 扩展类，包含从其他开源项目提取的class
- * XMaterial: 从xseries项目提取 https://github.com/CryptoMorin/XSeries 提取版本8.5.0.1
- */
-package com.handy.lib.expand;
--- src/main/java/com/handy/lib/util/ItemStackUtil.java
+++ src/main/java/com/handy/lib/util/ItemStackUtil.java
@@ -3,7 +3,6 @@ package com.handy.lib.util;
 import com.handy.lib.constants.VersionCheckEnum;
 import com.handy.lib.core.CollUtil;
 import com.handy.lib.core.StrUtil;
-import com.handy.lib.expand.XMaterial;
 import org.bukkit.Material;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -13,10 +12,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * ItemStack工具类
@@ -365,11 +361,16 @@ public class ItemStackUtil {
         if (StrUtil.isEmpty(materialStr)) {
             return defaultMaterial;
         }
+        materialStr = materialStr.toUpperCase(Locale.ROOT);
         Material material = Material.getMaterial(materialStr);
         if (material != null) {
             return material;
         }
-        return XMaterial.matchXMaterial(materialStr).orElse(XMaterial.matchXMaterial(defaultMaterial)).parseMaterial();
+        material = Material.getMaterial("LEGACY_" + materialStr, true);
+        if (material != null) {
+            return material;
+        }
+        return defaultMaterial;
     }
 
     /**
